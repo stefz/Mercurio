b311d2b6bd175fbfde47aa705bf5ad339d33eb10
Stefano Zanetti <stefano.zanetti@pragmamark.org>
2015-10-28 00:10:35 +0100

    Fixed header

--- Pod/Classes/MEErrorHelperProtocol.h
+++ Pod/Classes/MEErrorHelperProtocol.h
@@ -1,9 +1,9 @@
 //
 //  MEErrorHelperProtocol.h
-//  Bricoman
+//  Mercurio
 //
 //  Created by Stefano Zanetti on 24/07/15.
-//  Copyright (c) 2015 RetAPPs srl. All rights reserved.
+//  Copyright (c) 2015 Stefano Zanetti. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
